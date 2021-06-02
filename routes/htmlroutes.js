9c9a63af599487fc0519c71c2471c87cb2a19a85
ksdevinney <katie@devinney.org>
2021-06-02 11:54:17 -0600

    html routes

---
+++ routes/htmlroutes.js
@@ -0,0 +1,20 @@
+const app = require("express").Router();
+const path = require("path");
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+});
+
+app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+module.exports = app;
