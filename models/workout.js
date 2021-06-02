34c3246bb2c13c025e331c2c126935b8e5a41e49
ksdevinney <katie@devinney.org>
2021-06-02 11:40:53 -0600

    workout model

--- models/workout.js
+++ models/workout.js
@@ -2,9 +2,47 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: true,
+
+            },
+            name: {
+                type: String,
+                required: true,
+
+            },
+            duration: {
+                type: Number,
+                required: true,
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
     // name
 
     // cardio or strength
 
     // duration
 })
+
+const Workout = mongoose.model("Workout", workoutSchema)
+
+module.exports = Workout
