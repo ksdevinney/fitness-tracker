039ed1c4a62ec9bbb66bc82f99dcaf80174b8b1b
ksdevinney <katie@devinney.org>
2021-06-02 11:48:15 -0600

    server.js

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
 const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose.connect("mongoDB://localhost/fitness-tracker")
+
+app.use(require("./routes/apiroutes.js"))
+app.use(require("./routes/htmlroutes.js"))
+app.listen(PORT, function(){
+    console.log("App listening on PORT", PORT)
+})
